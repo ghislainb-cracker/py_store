fab43e5a0075e546e3b78d9a2a129244f09b0995
Byimbo Ghsilain <byimbog250@gmail.com>
2026-08-11 17:12:50 +0200

    added timer project

---
+++ stage_2/Day_8/timer.py
@@ -0,0 +1,12 @@
+import time
+
+user_seconds = int(input("Input time in seconds: "))
+
+for x in range(user_seconds, 0, -1):
+    seconds = x % 60
+    minutes = int(x/60)%60
+    hours = int(x / 3600)
+
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("Time is up bro!")
